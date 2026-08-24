d9a10fbc2889e0da2477821dfcb44478ce68c8cd
Nils0xFF <mail@nilsgeschwin.de>
2026-08-24 22:06:51 +0200

    chore(*): added correct base path again :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig, fontProviders } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    base: "/homepage",
     plugins: [tailwindcss()],
   },
   fonts: [
