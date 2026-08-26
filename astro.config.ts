c03ebe464b37f3b6e652234cc12eaa68217b7c26
Nils Geschwinde <nils.geschwinde@inside-m2m.de>
2026-08-26 12:21:31 +0200

    chore(*): setup linter, parser, formatter and pre commits, also started adding SEO and env support

---
+++ astro.config.ts
@@ -0,0 +1,52 @@
+// @ts-check
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
+const SITE_URL = process.env.SITE_URL;
+// https://astro.build/config
+export default defineConfig({
+	site: SITE_URL ?? "https://localhost:4321",
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "Inter",
+			cssVariable: "--font-inter",
+			options: {
+				variants: [
+					{
+						weight: "100 900",
+						style: "normal",
+						src: ["./src/assets/fonts/Inter.ttf"],
+					},
+					{
+						weight: "100 900",
+						style: "italic",
+						src: ["./src/assets/fonts/Inter-Italic.ttf"],
+					},
+				],
+			},
+		},
+		{
+			provider: fontProviders.local(),
+			name: "CalSans",
+			cssVariable: "--font-cal",
+			options: {
+				variants: [
+					{
+						weight: "100 900",
+						style: "normal",
+						src: ["./src/assets/fonts/CalSans-Regular.ttf"],
+					},
+				],
+			},
+		},
+	],
+
+	integrations: [sitemap()],
+});
